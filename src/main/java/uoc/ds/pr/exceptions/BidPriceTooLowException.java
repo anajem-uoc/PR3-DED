95c337ed16cde352417902f159bdfad57f7331c8
Anajem <ali.najem04@gmail.com>
2025-06-16 23:41:36 +0200

    add classes and exception

---
+++ src/main/java/uoc/ds/pr/exceptions/BidPriceTooLowException.java
@@ -0,0 +1,4 @@
+package uoc.ds.pr.exceptions;
+
+public class BidPriceTooLowException extends DSException {
+}
---
+++ src/main/java/uoc/ds/pr/exceptions/CatalogedCardAlreadyLentException.java
@@ -0,0 +1,4 @@
+package uoc.ds.pr.exceptions;
+
+public class CatalogedCardAlreadyLentException extends DSException {
+}
